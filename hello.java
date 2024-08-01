974c91dbb3d4b59b9b5e98b601f2ddba1d2e2247
twesto <93481545+twesto@users.noreply.github.com>
2024-08-01 11:15:25 +0400

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+
+
+    public static void main( String[] args) {
+
+        System.out.println("Author: Omar");
+        System.out.println("Jenkins Project - Explaining Git Project Compilation");
+
+    }
+
+
+} 
